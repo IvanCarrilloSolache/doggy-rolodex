147a655cc777ff15d7742eace0c64138660dd846
IvanCarrilloSolache <carrillo.solache@gmail.com>
2020-01-01 23:17:42 -0600

    refactoring - implemented state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//a class component allows you to display dinamic content
 class App extends Component {
+  //state === object with properties that can br accesed from anywhere within the class
+  constructor() {
+    // super calls the crostructor method on the component and give us access to this state
+    super();
+      this.state = {
+        string: "Hello Ivandcs"
+      };
+  }
   render () {
     return (
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello
+          { this.state.string }
         </p>
         <button>Change Text</button>
       </header>
