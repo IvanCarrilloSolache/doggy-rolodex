7d3923cbbdee4dafe03228fb71650fc557a0ebbc
IvanCarrilloSolache <carrillo.solache@gmail.com>
2020-01-04 00:47:41 -0600

    architecting App - implemented card-list component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { CardList } from "./components/card-list/card-list.component";
 import './App.css';
 
-//a class component allows you to display dinamic content
+//class component allows you to display dynamic content
 class App extends Component {
   //state === object with properties that can br accesed from anywhere within the class
   constructor() {
@@ -9,23 +10,24 @@ class App extends Component {
     super();
       this.state = {
         doggies: []
-      };
+      }; 
   }
   //renders component into dom/calls block of code
   componentDidMount() {
-    //returns a promise
+    //returns a promise/fetch users from API
     fetch("https:jsonplaceholder.typicode.com/users")
       //returns response in json format for JS to understand
       .then(response => response.json())
-      //returns new promise with body of users array/updates doggiesproperty with users array
+      //returns new promise with body of users array/updates doggies property with users array
       .then(users => this.setState({ doggies: users }));
   }
   
   render () {
     return (
       <div className="App">
-        {this.state.doggies.map(monster => (
-          <h1 key={monster.name}> {monster.name} </h1>
+      <CardList name="Wooca"/>
+        {this.state.doggies.map(doggy => (
+          <h1 key={doggy.name}> {doggy.name} </h1>
         ))}
       </div>
     );
