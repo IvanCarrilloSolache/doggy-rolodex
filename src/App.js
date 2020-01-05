eca45682523cf69090f573963daf20533d164fda
IvanCarrilloSolache <carrillo.solache@gmail.com>
2020-01-05 00:40:02 -0600

    refactor c-s/app comp - pass in monsters prop from appComp/move mapping over *appComp to c-sComp*

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 
 //class component allows you to display dynamic content
 class App extends Component {
-  //state === object with properties that can br accesed from anywhere within the class
+  //state => object with properties that can br accesed from anywhere within the class
   constructor() {
-    // super calls the crostructor method on the component and give us access to this state
+    //super calls the crostructor method on the component and give us access to this state
     super();
       this.state = {
         monsters: []
@@ -25,10 +25,9 @@ class App extends Component {
   render () {
     return (
       <div className="App">
-        <CardList name="Ivan"> 
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.name}> {monster.name} </h1>
-        ))}
+        {/*props => parameter pass in CardList/logs out object of properties into component*/}
+        <CardList monsters={this.state.monsters}> 
+          
         </CardList>
       </div>
     );
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import './card-list.styles.css';
-
+//prop is parameter we get from functional comp
 export const CardList = props => {
-    return <div className= "card-list">{props.children}</div>;
+    //children => pass between brakets of comp call
+    return <div className= "card-list">
+        {/*returns the return of passed func iterated over every arr's element*/}
+        {/*unique keys let React know what elements need update if arr's elements value changes */}
+        {props.monsters.map(monster => (
+            <h1 key={monster.name}> {monster.name} </h1>
+        ))}
+    </div>;
 };
