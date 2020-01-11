881a88bdda79be6d252eebd5a1dccf8e0986dd7a
IvanCarrilloSolache <carrillo.solache@gmail.com>
2020-01-11 10:25:20 -0600

    Updates

---
+++ precache-manifest.624f8e78834b9db6147d07a61f6a9270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef6779e2dab437c03c6f1a8bce2b996",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "5b905473f989d5676fcf",
+    "url": "/robots-rolodex/static/css/main.6f51f06a.chunk.css"
+  },
+  {
+    "revision": "39a2bd0350d99700a34c",
+    "url": "/robots-rolodex/static/js/2.7f4a0372.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robots-rolodex/static/js/2.7f4a0372.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b905473f989d5676fcf",
+    "url": "/robots-rolodex/static/js/main.51da2c3a.chunk.js"
+  },
+  {
+    "revision": "e10dd8668fc04e7d61fa",
+    "url": "/robots-rolodex/static/js/runtime-main.24f364d3.js"
+  }
+]);
