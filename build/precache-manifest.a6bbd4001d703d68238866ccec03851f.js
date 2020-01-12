2065a76a54463ad468763b6fa609245e3c5bfae6
IvanCarrilloSolache <carrillo.solache@gmail.com>
2020-01-11 22:08:19 -0600

    adding files for github pages

---
+++ build/precache-manifest.a6bbd4001d703d68238866ccec03851f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7eed02bd37ee09e9aecff9217a079ac",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "6e9ca25c71916b3f6d48",
+    "url": "/robots-rolodex/static/css/main.3ab0e941.chunk.css"
+  },
+  {
+    "revision": "39a2bd0350d99700a34c",
+    "url": "/robots-rolodex/static/js/2.7f4a0372.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robots-rolodex/static/js/2.7f4a0372.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e9ca25c71916b3f6d48",
+    "url": "/robots-rolodex/static/js/main.51da2c3a.chunk.js"
+  },
+  {
+    "revision": "e10dd8668fc04e7d61fa",
+    "url": "/robots-rolodex/static/js/runtime-main.24f364d3.js"
+  }
+]);
